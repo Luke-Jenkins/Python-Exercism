20dade8cb81c694776662ef981a9467890b21b17
Luke-Jenkins <luke.b.jenkins7@gmail.com>
2021-05-31 14:14:14 -0500

    Updated solutions to all problem to be more pythonic.

--- high-scores/high_scores.py
+++ high-scores/high_scores.py
@@ -1,21 +1,10 @@
 def latest(scores):
-    return scores[len(scores) - 1]
+    return scores[-1]
 
 
 def personal_best(scores):
-    scores.sort()
-    return scores[len(scores) - 1]
+    return max(scores)
 
 
 def personal_top_three(scores):
-    scores.sort(reverse=True)
-    top_scores = []
-
-    count = len(scores)
-    if count > 3:
-        count = 3
-
-    for i in range(count):
-        top_scores.append(scores[i])
-
-    return top_scores
+    return sorted(scores, reverse=True)[:3]
