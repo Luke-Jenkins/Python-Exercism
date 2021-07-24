8faeaacbd5d31eff7c8674ec58d4e5fc82d43bae
Luke-Jenkins <luke.b.jenkins7@gmail.com>
2021-07-24 18:42:08 -0500

    CAdded solution to distance function

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,2 +1,15 @@
+# returns hamming distance value
+
 def distance(strand_a, strand_b):
-    pass
+    if len(strand_a) != len(strand_b):
+        raise ValueError('Strands are not equal lengths.')
+
+    distance = 0
+
+    if strand_a == strand_b:
+        return distance
+    else:
+        for i in range(len(strand_a)):
+            if strand_a[i] != strand_b[i]:
+                distance += 1
+        return distance
