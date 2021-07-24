4957ab4730fd76a3a7a128bf6e40084a3db3fa6f
Luke-Jenkins <luke.b.jenkins7@gmail.com>
2021-07-24 17:28:40 -0500

    Added solution for Matrix exercise

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,9 +1,24 @@
 class Matrix:
     def __init__(self, matrix_string):
-        pass
+        # variables
+        self.rows = []
+        self.columns = []
+        # constant
+        self.row_count = matrix_string.count('\n') + 1
+
+        # split the input into arrays of rows
+        self.input = matrix_string.split('\n')
+
+        # convert arrays of rows from str to int
+        for i in range(self.row_count):
+            self.rows.append(self.input[i].split(' '))
+            self.rows[i] = [int(j) for j in self.rows[i]]
 
     def row(self, index):
-        pass
+        return self.rows[index - 1]
 
     def column(self, index):
-        pass
+        for i in range(len(self.rows)):
+            self.columns.append(self.rows[i][index - 1])
+
+        return self.columns
