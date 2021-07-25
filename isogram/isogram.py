9bb2dcdb187f1e48292a7f4ff768cb27a95a3d1b
Luke-Jenkins <luke.b.jenkins7@gmail.com>
2021-07-25 00:20:49 -0500

    Added solution to is_isogram function

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,2 +1,10 @@
 def is_isogram(string):
-    pass
+    string = sorted(string.lower())
+
+    for i, letter in enumerate(string):
+        if i == 0:
+            pass
+        elif letter == string[i - 1] and letter.isalpha() is True:
+            return False
+
+    return True
