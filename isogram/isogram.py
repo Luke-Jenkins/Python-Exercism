ddea6b1839a2dd27f790345c36d54eb0c27fd4a6
Luke-Jenkins <luke.b.jenkins7@gmail.com>
2021-07-25 00:45:34 -0500

    Added comments for later reflection

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,3 +1,7 @@
 def is_isogram(string):
+    # converts string to array of lower alpha chars
     string = [letter.lower() for letter in string if letter.isalpha()]
+    # returns boolan comparison of set(string) vs list(string)
+    # set(string) returns only unique items
+    # list(string) returns all items
     return len(set(string)) == len(list(string))
