31a17d7e82a799245ebaee620599309c50e27bdc
Luke-Jenkins <luke.b.jenkins7@gmail.com>
2021-07-25 00:42:32 -0500

    Updated code for more pythonic submission

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,10 +1,3 @@
 def is_isogram(string):
-    string = sorted(string.lower())
-
-    for i, letter in enumerate(string):
-        if i == 0:
-            pass
-        elif letter == string[i - 1] and letter.isalpha() is True:
-            return False
-
-    return True
+    string = [letter.lower() for letter in string if letter.isalpha()]
+    return len(set(string)) == len(list(string))
